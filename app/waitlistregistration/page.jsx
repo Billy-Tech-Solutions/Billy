a0d9801b10d5a365e5ad014dfd43bbc14acd0df3
Patty Arao <patricia_arao@dlsu.edu.ph>
2023-11-28 02:08:00 +0800

    fixed ui of howitworks and waitlistreg

--- app/waitlistregistration/page.jsx
+++ app/waitlistregistration/page.jsx
@@ -2,81 +2,57 @@
 
 import { useState } from "react";
 
+//images
+import logo from "@/public/billy-logo.png"
 //icons
 import { RiArrowDownSFill } from "react-icons/ri";
+import Image from "next/image";
 
 const WaitlistRegistration = () => {
-  const [userType, setUserType] = useState("");
-  const [showUserDropdown, setShowUserDropdown] = useState(false);
-
-  const handleShowUserDropdown = () => {
-    setShowUserDropdown(!showUserDropdown);
-  };
   return (
-    <main className="m-0 w-full h-[85vh] flex flex-col md:flex-row items-center justify-center md:justify-evenly">
-      <h1 className="font-black text-[60px] uppercase text-purple-main">
-        billy
-      </h1>
-      <div className="w-[90%] md:w-[40%] h-fit p-8 flex flex-col gap-8 items-center justify-center bg-pink-secondary">
-        <p className="w-full text-pink-main font-bold text-xl">
-          Join our waitlist!
-        </p>
-        <div className="w-full flex flex-col gap-4">
-          <div className="w-full flex flex-col gap-0.5">
-            <p className="text-xs text-black/60">Email</p>
-            <input
-              type="text"
-              className="focus:outline-none px-4 py-2 text-xs rounded-md border border-gray-main drop-shadow-sm"
-            />
-          </div>
-          <div className="w-full flex flex-col gap-0.5">
-            <p className="text-xs text-black/60">Company</p>
-            <input
-              type="text"
-              className="focus:outline-none px-4 py-2 text-xs rounded-md border border-gray-main drop-shadow-sm"
-            />
-          </div>
-          <div className="w-full flex flex-col gap-0.5">
-            <h2 className="text-xs text-black/60">How will you use Billy?</h2>
-            <div className="w-full">
-              <button
-                className="w-full mb-1 px-4 py-2 flex justify-between items-center text-xs rounded-md bg-white border border-gray-main drop-shadow-sm"
-                onClick={(e) => handleShowUserDropdown()}
-              >
-                <p className="">As a/an {userType}</p>
-                <RiArrowDownSFill />
-              </button>
-              {showUserDropdown === true ? (
-                <div className="absolute z-10 w-[285px] md:w-[420px] py-4 flex flex-col bg-white text-pink-main text-xs font-bold rounded-md">
-                  <button
-                    value="Advertiser"
-                    onClick={(e) => {
-                      setUserType(e.target.value);
-                      handleShowUserDropdown();
-                    }}
-                    className="p-2 text-left hover:bg-purple-main/50"
-                  >
-                    Advertiser
-                  </button>
-                  <button
-                    value="Ad Space Owner"
-                    onClick={(e) => {
-                      setUserType(e.target.value);
-                      handleShowUserDropdown();
-                    }}
-                    className="p-2 text-left hover:bg-purple-main/50"
-                  >
-                    Ad Space Owner
-                  </button>
+    <main className="m-0 w-full h-[85vh] flex flex-col lg:flex-row items-center justify-evenly">
+      <div className="flex items-center font-black text-[60px] md:text-[100px]">
+        <Image src={logo} width={300} height={300} alt="billy logo" className="w-[100px] lg:w-[300px]"/>
+        <h1 className="uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-main to-pink-main">BILLY</h1>
+      </div>
+      <div className="w-[90%] md:w-[60%] lg:w-[25%] h-[50vh] px-10 flex flex-col items-center justify-center bg-purple-secondary rounded-lg drop-shadow-lg">
+        <div className="w-full h-full flex flex-col items-center justify-evenly">
+          <p className="w-full text-pink-main font-bold text-xl">
+            Join our waitlist!
+          </p>
+          <div className="w-full flex flex-col gap-6 items-center justify-center ">
+            <div className="w-full flex flex-col gap-0.5">
+              <p className="text-xs text-gray-main">Email</p>
+              <input
+                type="text"
+                className="focus:outline-none w-full px-4 py-2 text-xs rounded-md border border-gray-main drop-shadow-sm"
+              />
+            </div>
+            <div className="w-full flex flex-col gap-0.5">
+              <p className="text-xs text-gray-main">Company</p>
+              <input
+                type="text"
+                className="focus:outline-none w-full px-4 py-2 text-xs rounded-md border border-gray-main drop-shadow-sm"
+              />
+            </div>
+            <div className="w-full flex flex-col gap-2 text-xs text-gray-main">
+              <h2>How will you use Billy?</h2>
+              <div className="w-full flex items-center gap-4">
+                <div className="flex items-center gap-1">
+                  <input type="radio" />
+                  <p>Advertiser</p>
                 </div>
-              ) : null}
+                <div className="flex items-center gap-1">
+                  <input type="radio" />
+                  <p>Ad Space Provider</p>
+                </div>
+              </div>
             </div>
           </div>
+          <button className="w-full h-fit bg-gradient-to-r from-purple-main to-pink-main text-white p-1.5 rounded-md font-bold text-xl drop-shadow-xl">
+              Register
+            </button>
         </div>
-
-        <button className="w-1/2 h-fit bg-button-main text-white p-1.5 rounded-md font-bold text-xl drop-shadow-xl">
-          Register
-        </button>
       </div>
     </main>
   );
